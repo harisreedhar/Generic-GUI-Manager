12ac56a178744e597510e315626fcfcee6ba762d
h4harisreedhar.s.s@gmail.com <46858047+harisreedhar@users.noreply.github.com>
2021-11-01 12:44:07 +0530

    add pre and post commands
    removed demo (not relavant anymore)

--- source/main.py
+++ source/main.py
@@ -3,134 +3,118 @@ import subprocess
 import tkinter as tk
 from tkinter import filedialog, font as tkFont
 
+
 class Theme:
-    colorA_1 = "#282c34"
-    colorA_2 = "#333842"
-    colorA_3 = "#3b4048"
-    colorA_4 = "#e79d28"
-    colorB_1 = "#e06c70"
-    colorB_2 = "#abb2bf"
-    colorB_3 = "#469aef"
-    colorB_4 = "#98c379"
-    buttonWidth = 18
-    fieldWidth = 60
+    def __init__(self):
+        self.a1 = "#282c34"
+        self.a2 = "#333842"
+        self.a3 = "#3b4048"
+        self.a4 = "#e79d28"
+        self.b1 = "#e06c70"
+        self.b2 = "#abb2bf"
+        self.b3 = "#469aef"
+        self.b4 = "#98c379"
+        self.fieldWidth = 60
+        self.labelWidth = 16
+        self.runButtonWidth = 18
+        self.fontA = tkFont.Font(family='helvetica', size=13)
+        self.fontB = tkFont.Font(family='helvetica', size=14, weight=tkFont.BOLD)
+        self.fontC = tkFont.Font(family='helvetica', size=12, slant='italic')
+        self.settings = {
+            'root':{'bg':self.a1},
+            'frame':{'bg':self.a1},
+            'label':{'bg':self.a1, 'fg':self.b2},
+            'label_frame':{'bg':self.a1, 'fg':self.b1},
+            'menu':{'bg':self.a2, 'fg':self.b2, 'activebackground':self.b3},
+            'text_field':{'highlightthickness':0, 'bg':self.a3, 'fg':self.b4},
+            'button_1':{'highlightthickness':0, 'bg':self.a2, 'fg':self.b2, 'activebackground':self.b3},
+            'run_button':{'highlightthickness':0, 'bg':self.a2, 'fg':self.b2, 'activebackground':self.b1},
+            'check_button':{'highlightthickness':0, 'bg':self.a1, 'fg':self.b2, 'activebackground':self.a1, 'activeforeground':self.b3, 'selectcolor':self.a2}
+        }
 
-# https://stackoverflow.com/questions/4266566/stardand-context-menu-in-python-tkinter-text-widget-when-mouse-right-button-is-p
-def rClicker(e):
-    try:
-        def rClick_Copy(e, apnd=0):
-            e.widget.event_generate('<Control-c>')
-        def rClick_Cut(e):
-            e.widget.event_generate('<Control-x>')
-        def rClick_Paste(e):
-            e.widget.event_generate('<Control-v>')
-        e.widget.focus()
-        nclst=[
-               (' Cut', lambda e=e: rClick_Cut(e)),
-               (' Copy', lambda e=e: rClick_Copy(e)),
-               (' Paste', lambda e=e: rClick_Paste(e)),
-               ]
-        rmenu = tk.Menu(None, tearoff=0, takefocus=0)
-        rmenu.config(bg=Theme.colorA_2, fg=Theme.colorB_2, activebackground=Theme.colorB_3)
-        for (txt, cmd) in nclst:
-            rmenu.add_command(label=txt, command=cmd)
-        rmenu.tk_popup(e.x_root+40, e.y_root+10,entry="0")
-    except tk.TclError:
-        pass
-    return "break"
-
-def rClickbinder(r):
-    try:
-        for b in [ 'Text', 'Entry', 'Listbox', 'Label']: #
-            r.bind_class(b, sequence='<Button-3>',
-                         func=rClicker, add='')
-    except tk.TclError:
-        pass
-
-def createInput(frame, index=0, name="STRING", type=None, value=None):
-    fieldText = tk.StringVar()
-    field = tk.Entry(frame, textvariable=fieldText, width=Theme.fieldWidth)
-    field.config(highlightthickness=0, bg=Theme.colorA_3, fg=Theme.colorB_4)
-    field.bind('<Button-3>',rClicker, add='')
-    field.grid(row=index, column=1, padx=(0,5), pady=(5,5))
-    if value is not None: fieldText.set(value)
-
-    if type == "FILE":
-        button = tk.Button(frame, width=Theme.buttonWidth)
-        button["text"] = name
-        button["command"] = lambda: fieldText.set(filedialog.askopenfilename(initialdir=value, title="Select file"))
-        button.config(highlightthickness=0, bg=Theme.colorA_2, fg=Theme.colorB_2, activebackground=Theme.colorB_3)
-        button.grid(row=index, column=0, padx=(5,5), pady=(5,5))
-    elif type == "DIRECTORY":
-        button = tk.Button(frame, width=Theme.buttonWidth)
-        button["text"] = name
-        button["command"] = lambda: fieldText.set(filedialog.askdirectory(initialdir=value, title="Select directory"))
-        button.config(highlightthickness=0, bg=Theme.colorA_2, fg=Theme.colorB_2, activebackground=Theme.colorB_3)
-        button.grid(row=index, column=0, padx=(5,5), pady=(5,5))
-    elif type == "STRING":
-        label = tk.Label(frame, text=name, width=Theme.buttonWidth)
-        label.config(bg=Theme.colorA_1, fg=Theme.colorB_2)
-        label.grid(row=index, column=0, padx=(5,5), pady=(5,5))
-    return fieldText
 
 class App:
-    def __init__(self, root):
-        root.title("Py-cli2gui")
-
-        with open('config.json') as json_file:
-            self.projects = json.load(json_file)
-
+    def __init__(self, root, theme):
+        self.root = root
+        self.theme = theme
+        self.projects = self.getProjects('config.json')
+        self.projectName = tk.StringVar()
+        self.projectName.set(list(self.projects.keys())[0])
+        self.projectName.trace("w", self.setCurrentProject)
+        self.runInProjectPath = tk.BooleanVar(value=True)
         self.fields = []
+        self.frameA()
+        self.frameB()
+        self.setCurrentProject()
 
-        frame = tk.Frame(root, relief="groove", bg=Theme.colorA_1)
-        _font = tkFont.Font(family='helvetica', size=14, weight=tkFont.BOLD)
+    def frameA(self):
+        frame =  tk.Frame(self.root, relief="groove")
+        frame.config(**self.theme.settings.get('frame'))
 
-        self.currentProjectName = tk.StringVar(frame)
-        self.currentProjectName.set(list(self.projects.keys())[0])
-        self.currentProjectName.trace("w", self.setCurrentProject)
-        optionMenu = tk.OptionMenu(frame, self.currentProjectName, *self.projects.keys())
-        optionMenu.config(bg=Theme.colorA_2, fg=Theme.colorB_2, highlightthickness=0, activebackground=Theme.colorB_3)
-        optionMenu["menu"].config(bg=Theme.colorA_2, fg=Theme.colorB_2, activebackground=Theme.colorB_3)
-        optionMenu["font"] = _font
+        optionMenu = tk.OptionMenu(frame, self.projectName, *self.projects.keys())
+        optionMenu.config(**self.theme.settings.get('menu'))
+        optionMenu.config(width=30)
+        optionMenu["menu"].config(**self.theme.settings.get('menu'))
+        optionMenu.config(highlightthickness=0)
+        optionMenu["font"] = self.theme.fontB
         optionMenu.grid(row=0, column=0)
 
-        runButton = tk.Button(frame, fg=Theme.colorB_2, bg=Theme.colorA_2, activebackground=Theme.colorB_1, width=Theme.buttonWidth)
+        runButton = tk.Button(frame, width=self.theme.runButtonWidth)
+        runButton.config(**self.theme.settings.get('run_button'))
         runButton["text"] = 'RUN'
         runButton["command"] = self.runCommand
-        runButton["font"] = _font
-        runButton.config(highlightthickness=0)
-        runButton.grid(row=0, column=1, padx=5)
+        runButton["font"] = self.theme.fontB
+        runButton.grid(row=0, column=1, padx=6)
 
-        self.inProjectDir = tk.BooleanVar(value=True)
-        inProjectDirButton = tk.Checkbutton(frame, variable=self.inProjectDir, bg=Theme.colorA_1, fg=Theme.colorB_2)
-        inProjectDirButton["text"] = "Run in project directory"
-        inProjectDirButton.config(highlightthickness=0, activebackground=Theme.colorA_1, activeforeground=Theme.colorB_3, selectcolor=Theme.colorA_2)
-        inProjectDirButton.grid(row=1, column=1, padx=2)
+        self.description = tk.Label(frame, text="Face swaping project")
+        self.description.config(**self.theme.settings.get('label'))
+        self.description["font"] = self.theme.fontC
+        self.description.grid(row=1, column=0, padx=2, pady=2)
 
-        frame.pack(padx=10,pady=(10,0))
+        checkButton = tk.Checkbutton(frame, variable=self.runInProjectPath)
+        checkButton["text"] = "Run in project directory"
+        checkButton.config(**self.theme.settings.get('check_button'))
+        checkButton.grid(row=1, column=1, sticky='NW')
 
-        fileFrame = tk.LabelFrame(root, text=" Path settings ", relief="groove", bg=Theme.colorA_1, fg=Theme.colorB_1)
+        frame.pack(padx=6, pady=(12,0))
 
-        self.projectDirectory = createInput(fileFrame, index=0, name="Project directory", type="DIRECTORY")
-        self.filePath = createInput(fileFrame, index=1, name="File path", type="FILE")
-        self.pythonPath = createInput(fileFrame, index=2, name="Python path", type="FILE")
-        fileFrame.pack(fill="both", expand="yes", padx=10, pady=0)
+    def frameB(self):
+        frame = tk.LabelFrame(self.root, text=" Settings ", relief="groove")
+        frame.config(**self.theme.settings.get('label_frame'))
+        self.projectPath = self.createInput(frame, index=0, name="Project path", type="DIRECTORY")
+        self.filePath = self.createInput(frame, index=1, name="File path", type="FILE")
+        self.pythonPath = self.createInput(frame, index=2, name="Python path", type="FILE")
+        # self.preCommands = self.createInput(frame, index=3, name="Pre command", type="STRING", theme=self.theme)
+        # self.postCommands = self.createInput(frame, index=4, name="Post command", type="STRING", theme=self.theme)
+        frame.pack(fill="y", expand="yes", padx=6, pady=(0,6))
 
-        self.setCurrentProject()
+    def frameC(self):
+        self.dynamicFrame = tk.LabelFrame(self.root, text=" Arguments ", relief="groove")
+        self.dynamicFrame.config(**self.theme.settings.get('label_frame'))
+        for i, item in enumerate(self.currentProject['inputs']):
+            self.fields.append(self.createInput(self.dynamicFrame, index=i, **item))
+        self.dynamicFrame.pack(fill="y", expand="yes", padx=6, pady=6)
+
+    def removeFrame(self):
+        if hasattr(self, 'dynamicFrame'):
+            self.dynamicFrame.pack_forget()
+            self.dynamicFrame.destroy()
+        self.fields.clear()
 
     def setCurrentProject(self, *args):
         self.removeFrame()
-        self.currentProject = self.projects[self.currentProjectName.get()]
-        self.createInputs()
-        self.projectDirectory.set(self.currentProject['project path'])
+        self.currentProject = self.projects[self.projectName.get()]
+        self.frameC()
+        self.projectPath.set(self.currentProject['project path'])
         self.filePath.set(self.currentProject['file path'])
         self.pythonPath.set(self.currentProject['python path'])
+        self.description.configure(text="( " + self.currentProject.get('description') + " )")
+        # self.preCommands.set(self.currentProject.get('pre_commands'))
+        # self.postCommands.set(self.currentProject.get('post_commands'))
 
-    def createInputs(self):
-        self.argFrame = tk.LabelFrame(root, text=" Arguments ", relief="groove", bg=Theme.colorA_1, fg=Theme.colorB_1)
-        for i, item in enumerate(self.currentProject['inputs']):
-            self.fields.append(createInput(self.argFrame, index=i, **item))
-        self.argFrame.pack(fill="both", expand="yes", padx=10, pady=10)
+    def getProjects(self, configPath):
+        with open(configPath) as json_file:
+            return json.load(json_file)
 
     def generateCommand(self):
         env = self.pythonPath.get()
@@ -138,24 +122,99 @@ class App:
         for i, arg in enumerate(self.currentProject['arguments']):
             value = self.fields[i].get()
             command = " ".join([command, arg, value])
-        return command
-
-    def removeFrame(self):
-        if hasattr(self, 'argFrame'):
-            self.argFrame.pack_forget()
-            self.argFrame.destroy()
-        self.fields.clear()
+        cdCommand = f"cd {self.projectPath.get() if self.runInProjectPath.get() else '.'}"
+        preCommands = self.currentProject.get("pre_commands")
+        if isinstance(preCommands, list):
+            preCommands = " && ".join(preCommands)
+        postCommands = self.currentProject.get("post_commands")
+        if isinstance(postCommands, list):
+            postCommands = " && ".join(postCommands)
+        subprocess.run(f" echo {preCommands}", shell=True, stdout=True)
+        commands = [cdCommand, preCommands, command, postCommands]
+        commands = filter(None, commands)
+        finalCommand = " && ".join(commands)
+        return finalCommand
 
     def runCommand(self):
         command = self.generateCommand()
-        path = self.projectDirectory.get() if self.inProjectDir.get() else "."
-        subprocess.run(f"cd {path} && {command}", shell=True, stdout=True)
+        print("---------------------- Command --------------------------------")
+        print(command)
+        print("---------------------- Execution started ----------------------")
+        subprocess.run(command, shell=True, stdout=True)
+        print("---------------------- Execution ended ------------------------")
+
+    # https://stackoverflow.com/questions/4266566/stardand-context-menu-in-python-tkinter-text-widget-when-mouse-right-button-is-p
+    def rClickMenu(self, e):
+        try:
+            def rCopy(e, apnd=0):
+                e.widget.event_generate('<Control-c>')
+            def rCut(e):
+                e.widget.event_generate('<Control-x>')
+            def rPaste(e):
+                e.widget.event_generate('<Control-v>')
+            e.widget.focus()
+            nclst = [
+                (' Cut', lambda e=e: rCut(e)),
+                (' Copy', lambda e=e: rCopy(e)),
+                (' Paste', lambda e=e: rPaste(e)),]
+            rMenu = tk.Menu(None, tearoff=0, takefocus=0)
+            rMenu.config(**self.theme.settings.get('menu'))
+            for (txt, cmd) in nclst:
+                rMenu.add_command(label=txt, command=cmd)
+            rMenu.tk_popup(e.x_root + 40, e.y_root + 10, entry="0")
+        except tk.TclError:
+            pass
+        return "break"
+
+    def rClickBinder(self, r):
+        try:
+            for b in [ 'Text', 'Entry', 'Listbox', 'Label']:
+                r.bind_class(b, sequence='<Button-3>', func=self.rClickMenu, add='')
+        except tk.TclError:
+            pass
+
+    def createInput(self, frame, index=0, name="", type="STRING", value=None):
+        text = tk.StringVar()
+        if isinstance(value, (str, int, float)):
+            text.set(value)
+        label = tk.Label(frame, text=name, width=self.theme.labelWidth, anchor="e")
+        label.config(**self.theme.settings.get('label'))
+        label.grid(row=index, column=0, padx=2, pady=2, sticky="E")
+        if type == "FILE":
+            button = tk.Button(frame, anchor="w")
+            button["text"] = "..."
+            button["command"] = lambda: text.set(filedialog.askopenfilename(title="Select file"))
+            button.config(**self.theme.settings.get('button_1'))
+            button.grid(row=index, column=2, padx=2, pady=2)
+        elif type == "DIRECTORY":
+            button = tk.Button(frame, anchor="w")
+            button["text"] = "..."
+            button["command"] = lambda: text.set(filedialog.askdirectory(title="Select directory"))
+            button.config(**self.theme.settings.get('button_1'))
+            button.grid(row=index, column=2, padx=2, pady=2)
+        elif type == "LIST":
+            menu = tk.OptionMenu(frame, text, *value)
+            menu.config(**self.theme.settings.get('menu'))
+            menu["menu"].config(**self.theme.settings.get('menu'))
+            menu.config(highlightthickness=0)
+            menu.grid(row=index, column=1, padx=(0,2), pady=2, sticky="W")
+            text.set(value[0])
+        elif type == "STRING":
+            pass
+        if type != "LIST":
+            field = tk.Entry(frame, textvariable=text, width=self.theme.fieldWidth)
+            field.config(**self.theme.settings.get('text_field'))
+            field.bind('<Button-3>', self.rClickMenu, add='')
+            field.grid(row=index, column=1, padx=(0,1), pady=2)
+        return text
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root["bg"] = Theme.colorA_1
-    _font = tkFont.Font(family='helvetica', size=13)
-    root.option_add("*font", _font)
+    theme = Theme()
+    root.title("Py-Cli2Gui")
+    root.config(**theme.settings.get('root'))
+    root.option_add("*font", theme.fontA)
     root.resizable(width=False, height=False)
-    app = App(root)
+    app = App(root, theme)
     root.mainloop()
