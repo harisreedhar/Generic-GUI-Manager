e5c4c165fd8c7dfb6d5e590a2d5454701458bbb6
h4harisreedhar.s.s@gmail.com <46858047+harisreedhar@users.noreply.github.com>
2021-11-04 13:29:52 +0530

    gui update
    * removed widget border to get flat look
    * added label instead of label frame
    * add dark and light theme

--- source/main.py
+++ source/main.py
@@ -6,18 +6,31 @@ import tkinter as tk
 from tkinter import filedialog, font as tkFont
 
 
-class Theme:
+class DARK:
+    a1 = "#25292f"
+    a2 = "#333842"
+    a3 = "#3b4048"
+    a4 = "#e79d28"
+    b1 = "#e06c70"
+    b2 = "#abb2bf"
+    b3 = "#469aef"
+    b4 = "#98c379"
+
+class LIGHT:
+    a1 = "#fffefe"
+    a2 = "#c6cddf"
+    a3 = "#ebedf2"
+    a4 = "#b630f4"
+    b1 = "#9199f9"
+    b2 = "#74839a"
+    b3 = "#a4a8de"
+    b4 = "#8f8e95"
+
+
+class Theme(DARK):
     def __init__(self):
-        self.a1 = "#282c34"
-        self.a2 = "#333842"
-        self.a3 = "#3b4048"
-        self.a4 = "#e79d28"
-        self.b1 = "#e06c70"
-        self.b2 = "#abb2bf"
-        self.b3 = "#469aef"
-        self.b4 = "#98c379"
         self.fieldWidth = 60
-        self.labelWidth = 16
+        self.labelWidth = 12
         self.runButtonWidth = 8
         self.fontA = tkFont.Font(family='helvetica', size=13)
         self.fontB = tkFont.Font(family='helvetica', size=14, weight=tkFont.BOLD)
@@ -26,6 +39,7 @@ class Theme:
             'root':{'bg':self.a1},
             'frame':{'bg':self.a1},
             'label':{'bg':self.a1, 'fg':self.b2},
+            'breaker':{'bg':self.a1, 'fg':self.b1},
             'label_frame':{'bg':self.a1, 'fg':self.b1},
             'menu':{'bg':self.a2, 'fg':self.b2, 'activebackground':self.b3},
             'text_field':{'highlightthickness':0, 'bg':self.a3, 'fg':self.b4},
@@ -46,19 +60,22 @@ class App:
         self.runInProjectPath = tk.BooleanVar(value=True)
         self.fields = []
         self.frameA()
+        self.breaker()
         self.frameB()
-        self.dynamicFrame = tk.LabelFrame(self.root, text=" Arguments ", relief="groove")
+        self.breaker()
+        self.dynamicFrame = tk.Frame(self.root)
         self.setCurrentProject()
+        self.breaker()
         self.frameD()
 
     def frameA(self):
-        frame =  tk.Frame(self.root, relief="groove")
+        frame =  tk.Frame(self.root)
         frame.config(**self.theme.settings.get('frame'))
 
         optionMenu = tk.OptionMenu(frame, self.projectName, *self.projects.keys())
-        optionMenu.config(**self.theme.settings.get('menu'))
+        optionMenu.config(**self.theme.settings.get('menu'), borderwidth=0)
         optionMenu.config(width=30)
-        optionMenu["menu"].config(**self.theme.settings.get('menu'))
+        optionMenu["menu"].config(**self.theme.settings.get('menu'), borderwidth=0)
         optionMenu.config(highlightthickness=0)
         optionMenu["font"] = self.theme.fontB
         optionMenu.grid(row=0, column=0)
@@ -68,24 +85,24 @@ class App:
         self.description["font"] = self.theme.fontC
         self.description.grid(row=1, column=0, padx=2, pady=2)
 
-        frame.pack(padx=6, pady=(12,0))
+        frame.pack(padx=24, pady=(18,0))
 
     def frameB(self):
-        frame = tk.LabelFrame(self.root, text=" Settings ", relief="groove")
-        frame.config(**self.theme.settings.get('label_frame'))
+        frame = tk.Frame(self.root)
+        frame.config(**self.theme.settings.get('frame'))
         self.projectPath = self.createInput(frame, index=0, name="Project path", type="DIRECTORY")
         self.filePath = self.createInput(frame, index=1, name="File path", type="FILE")
         self.pythonPath = self.createInput(frame, index=2, name="Python path", type="FILE")
-        frame.pack(fill="y", expand="yes", padx=6, pady=(0,6))
+        frame.pack(fill="y", expand="yes", padx=24, pady=0)
 
     def frameC(self):
-        self.dynamicFrame.config(**self.theme.settings.get('label_frame'))
+        self.dynamicFrame.config(**self.theme.settings.get('frame'))
         for i, item in enumerate(self.currentProject['inputs']):
             self.fields.append(self.createInput(self.dynamicFrame, index=i, **item))
-        self.dynamicFrame.pack(fill="y", expand="yes", padx=6, pady=6)
+        self.dynamicFrame.pack(fill="y", expand="yes", padx=24)
 
     def frameD(self):
-        frame =  tk.Frame(self.root, relief="groove")
+        frame =  tk.Frame(self.root)
         frame.config(**self.theme.settings.get('frame'))
 
         checkButton = tk.Checkbutton(frame, variable=self.runInProjectPath)
@@ -93,14 +110,18 @@ class App:
         checkButton.config(**self.theme.settings.get('check_button'))
         checkButton.grid(row=0, column=0, sticky='E')
 
-        runButton = tk.Button(frame, width=self.theme.runButtonWidth)
+        runButton = tk.Button(frame, width=self.theme.runButtonWidth, borderwidth=0)
         runButton.config(**self.theme.settings.get('run_button'))
         runButton["text"] = 'RUN'
         runButton["command"] = self.runCommand
         runButton["font"] = self.theme.fontB
         runButton.grid(row=0, column=1, padx=6, sticky='E')
 
-        frame.pack(padx=24, pady=(6,12), side=tk.RIGHT)
+        frame.pack(padx=24, pady=(0,18), side=tk.RIGHT)
+
+    def breaker(self):
+        breaker = tk.Label(self.root, text="_____"*15, **theme.settings.get("breaker"))
+        breaker.pack(padx=0, pady=(0,18))
 
     def removeFrame(self):
         for widget in self.dynamicFrame.winfo_children():
@@ -183,35 +204,36 @@ class App:
         text = tk.StringVar()
         if isinstance(value, (str, int, float)):
             text.set(value)
+        name = name[:12] + (name[12:] and '..')
         label = tk.Label(frame, text=name, width=self.theme.labelWidth, anchor="e")
         label.config(**self.theme.settings.get('label'))
         label.grid(row=index, column=0, padx=2, pady=2, sticky="E")
         if type == "FILE":
-            button = tk.Button(frame, anchor="w")
+            button = tk.Button(frame, anchor="n", borderwidth=0)
             button["text"] = "..."
             button["command"] = lambda: text.set(filedialog.askopenfilename(title="Select file"))
             button.config(**self.theme.settings.get('button_1'))
             button.grid(row=index, column=2, padx=2, pady=2)
         elif type == "DIRECTORY":
-            button = tk.Button(frame, anchor="w")
+            button = tk.Button(frame, anchor="n", borderwidth=0)
             button["text"] = "..."
             button["command"] = lambda: text.set(filedialog.askdirectory(title="Select directory"))
             button.config(**self.theme.settings.get('button_1'))
             button.grid(row=index, column=2, padx=2, pady=2)
         elif type == "LIST":
             menu = tk.OptionMenu(frame, text, *value)
-            menu.config(**self.theme.settings.get('menu'))
-            menu["menu"].config(**self.theme.settings.get('menu'))
+            menu.config(**self.theme.settings.get('menu'), borderwidth=0)
+            menu["menu"].config(**self.theme.settings.get('menu'), borderwidth=0)
             menu.config(highlightthickness=0)
-            menu.grid(row=index, column=1, padx=(0,2), pady=2, sticky="W")
+            menu.grid(row=index, column=1, padx=2, pady=2, sticky="W")
             text.set(value[0])
         elif type == "STRING":
             pass
         if type != "LIST":
-            field = tk.Entry(frame, textvariable=text, width=self.theme.fieldWidth)
+            field = tk.Entry(frame, textvariable=text, width=self.theme.fieldWidth, borderwidth=0)
             field.config(**self.theme.settings.get('text_field'))
             field.bind('<Button-3>', self.rClickMenu, add='')
-            field.grid(row=index, column=1, padx=(0,1), pady=2)
+            field.grid(row=index, column=1, padx=4, pady=2)
         return text
 
 
@@ -222,7 +244,5 @@ if __name__ == "__main__":
     root.config(**theme.settings.get('root'))
     root.option_add("*font", theme.fontA)
     root.resizable(width=False, height=False)
-    #root.rowconfigure(0, weight=1)
-    #root.columnconfigure(0, weight=1)
     app = App(root, theme)
     root.mainloop()
